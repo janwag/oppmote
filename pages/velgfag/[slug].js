70eba30949a59e8d2ec79a9b151de55773e573a4
sander <sandert.kodehode@gmail.com>
2022-11-17 00:18:03 +0100

    data added in cms, render field if value is more then 0

--- pages/velgfag/[slug].js
+++ pages/velgfag/[slug].js
@@ -1,6 +1,7 @@
 import groq from 'groq'
 import client from './client'
 import s from '../../styles/slug.module.css'
+import { useState } from 'react'
 
 export async function getStaticPaths() {
 	const respon = await client.fetch(groq`*[_type == 'data']`)
@@ -35,16 +36,63 @@ export async function getStaticProps(context) {
 	}
 }
 
+export const clickHandler = () => {
+	console.log(container.input.value)
+}
+
 export default function ProfilePage({ data }) {
+	console.log(data)
 	return (
-		<div className={s.container}>
-			<h1>{data.name}</h1>
-			<h2>{data.className}</h2>
-			{data.seminar > 0 ? (
-				<div className='seminar'>Seminar: {data.seminar}</div>
-			) : (
-				''
-			)}
-		</div>
+		<>
+			<div className={s.container}>
+				<h1>{data.name}</h1>
+
+				<h2>{data.className}</h2>
+
+				{data.arbeidsgrupper > 0 ? (
+					<p className='Arbeidsgrupper'>
+						Jeg har deltatt på <input type='number' /> av {data.arbeidsgrupper} arbeidsgrupper.
+					</p>
+				) : (
+					''
+				)}
+
+				{data.storgrupper > 0 ? (
+					<div className='storgrupper'>
+						Jeg har deltatt på <input type='number' /> av {data.storgrupper} storgrupper.
+					</div>
+				) : (
+					''
+				)}
+
+				{data.oppgaver > 0 ? (
+					<p className='oppgaver'>
+						Jeg har deltatt på <input type='number' /> av {data.oppgaver} oppgaver.
+					</p>
+				) : (
+					''
+				)}
+				{data.kommentering > 0 ? (
+					<p className='kommentering'>
+						Jeg har deltatt på <input type='number' /> av {data.kommentering} kommenteringer.
+					</p>
+				) : (
+					''
+				)}
+				{data.seminar > 0 ? (
+					<p className='seminar'>
+						Jeg har deltatt på <input type='number' /> av {data.seminar} seminar.
+					</p>
+				) : (
+					''
+				)}
+			</div>
+
+			<button
+				className={s.regn_ut}
+				onClick={clickHandler}>
+				Regn ut
+			</button>
+		</>
 	)
 }
