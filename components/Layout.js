ae8bf510d9ff41be5cb9bc775debb61309f36154
sander <sandert.kodehode@gmail.com>
2022-12-17 20:46:03 +0100

    deploy test

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,5 @@
 import Footer from '../components/footer'
 import Navbar from '/components/navbar.js'
-import s from '../styles/layout.module.css' // templet komponent som man wrapper rundt andre komponenter slik. Kan brukes flere ganger
 export default function Layout({ children }) {
 	return (
 		<>
