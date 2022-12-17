ffaae2a41003fa9ca9804f69b5157e0b5729ef63
sander <sandert.kodehode@gmail.com>
2022-12-17 20:48:05 +0100

    re deploy test

--- components/Layout.js
+++ components/Layout.js
@@ -2,10 +2,10 @@ import Footer from '../components/footer'
 import Navbar from '/components/navbar.js'
 export default function Layout({ children }) {
 	return (
-		<>
+		<div>
 			<Navbar />
-			{children}
+			<div>{children}</div>
 			<Footer />
-		</>
+		</div>
 	)
 }
