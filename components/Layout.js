ea73b63c068b4e0485a7b2e58d1bae4620afc6cf
sander <sandert.kodehode@gmail.com>
2022-12-17 20:24:39 +0100

    removed layout style file

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,11 @@
 import Footer from '../components/footer'
 import Navbar from '/components/navbar.js'
-import s from '../styles/layout.module.css'
-// templet komponent som man wrapper rundt andre komponenter slik. Kan brukes flere ganger
+import s from '../styles/layout.module.css' // templet komponent som man wrapper rundt andre komponenter slik. Kan brukes flere ganger
 export default function Layout({ children }) {
 	return (
 		<>
 			<Navbar />
-			<div className={s.container}>{children}</div>
+			{children}
 			<Footer />
 		</>
 	)
