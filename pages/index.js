50d50553a827ea523333bc493009886b4c8eb040
Sander Tonheim <sandert.kodehode@gmail.com>
2022-12-23 14:18:20 +0100

    added classnames

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,10 @@ import s from '../styles/Home.module.css'
 function Home() {
 	return (
 		<div className={s.container}>
-			<h1>
+			<h1 className={s.header}>
 				Regn ut din oppmøte <br /> prosent
 			</h1>
-			<div>
+			<div className={s.link}>
 				<Link href='/velgfag'>Velg Fag</Link>
 			</div>
 		</div>
