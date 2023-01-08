a41d85215bcd67a5f4bd40a9583b637a88aba182
Sander Tonheim <sandert.kodehode@gmail.com>
2023-01-08 02:23:05 +0100

    removed header component, caused trouble with footer and navbar placements

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,11 @@
-import '../styles/globals.css'
+import s from '../styles/globals.css'
 import Layout from '../components/Layout'
 import Head from 'next/head'
 function MyApp({ Component, pageProps }) {
 	return (
-		<>
-			<Head>
-				<title>Foks AS</title>
-				<meta
-					name='viewport'
-					content='width=device-width, initial-scale=1.0'
-				/>
-			</Head>
-			<Layout>
-				<Component {...pageProps} />
-			</Layout>
-		</>
+		<Layout>
+			<Component {...pageProps} />
+		</Layout>
 	)
 }
 
