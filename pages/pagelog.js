f83de604ba469115e3a0c597aa4d7e782c9db4ef
Sander Tonheim <sandert.kodehode@gmail.com>
2023-01-09 22:57:33 +0100

    added fetch data component for pagelog site

--- pages/pagelog.js
+++ pages/pagelog.js
@@ -2,19 +2,13 @@
 import s from '../styles/pagelog.module.css'
 import Link from 'next/link'
 import Header from '../components/Header'
+import Render from '../components/FetchChangeLog'
 
-function pagelog() {
+const pagelog = (data) => {
 	return (
 		<div className={s.container}>
 			<Header>Changelog</Header>
-			<ul>
-				<li>
-					04.11.2022: Lagt inn faget <Link href='https://www.uib.no/emne/JUS136'>JUS136</Link>.
-				</li>
-				<li>
-					15.11.2022: Endret antall oppgaver i faget <Link href='https://www.uib.no/emne/JUS135'>JUS135</Link> fra 10 til 15.
-				</li>
-			</ul>
+			<Render />
 		</div>
 	)
 }
