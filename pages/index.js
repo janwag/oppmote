a89125a91b58ec00f05a6b03960faabec365c57a
Sander Tonheim <sandert.kodehode@gmail.com>
2023-01-10 22:03:27 +0100

    added header component

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ function Home() {
 	return (
 		<div className={s.container}>
 			<div className={s.content}>
-				<Header text={'Regn ut din oppmøteprosent'} />
+				<Header>Regn ut din oppmøteprosent</Header>
 				<div className={s.link}>
 					<Link href='/velgfag'>Velg Fag</Link>
 				</div>
