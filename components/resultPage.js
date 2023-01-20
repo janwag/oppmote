d83d9b6740352e72339ca30fea3eeeb452c3733a
Sander Tonheim <sandert.kodehode@gmail.com>
2023-01-20 18:46:15 +0100

    removed usless code, added all props and tested

--- components/resultPage.js
+++ components/resultPage.js
@@ -2,47 +2,11 @@ import Link from 'next/link'
 import s from '../styles/result.module.css'
 import Header from './Header'
 
-const Result = ({ classGruppe1, classGruppe2 }) => {
+const Result = ({ classGruppe1, classGruppe2, userInputArbeidStorgrupper, userInputRest }) => {
 	console.log(classGruppe1)
 	console.log(classGruppe2)
-	// const array = gruppe1?.map((item) => {
-	// 	const array = item.class
-	// 	return array
-	// })
-
-	// const arraySum = (array) => {
-	// 	let arraySum = 0 // initialize sum
-
-	// 	// Iterate through all elements
-	// 	// and add them to sum
-	// 	for (let i = 0; i < array.length; i++) arraySum += array[i]
-
-	// 	return arraySum
-	// }
-	// // converting userInput object to array
-	// const userData = Object.values(userInput)
-	// // Driver code
-	// const numConverter = userData.map((str) => {
-	// 	// using map() to convert array of strings to numbers
-	// 	return parseInt(str)
-	// })
-
-	// let arr = numConverter
-
-	// const sum = (arr) => {
-	// 	let sum = 0 // initialize sum
-
-	// 	// Iterate through all elements
-	// 	// and add them to sum
-	// 	for (let i = 0; i < arr.length; i++) sum += arr[i]
-
-	// 	return sum
-	// }
-
-	// const percentCalc = (sum(arr) / arraySum(array)) * 100
-
-	// const result = Math.floor(percentCalc)
-
+	console.log(userInputArbeidStorgrupper)
+	console.log(userInputRest)
 	return (
 		<div className={s.container}>
 			{/* <div className={s.content}>
