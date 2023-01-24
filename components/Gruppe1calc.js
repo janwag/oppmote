dedc5fe75166048b13fd5dd8b9295a7d96601a98
Sander Tonheim <sandert.kodehode@gmail.com>
2023-01-24 16:44:28 +0100

    calculation done, right calulation way added

--- components/Gruppe1calc.js
+++ components/Gruppe1calc.js
@@ -2,11 +2,14 @@ export default function Gruppe1calc(cmsDataG1, userInputGroupe1) {
 	const classes = cmsDataG1?.map((item) => {
 		return item.class
 	})
+	console.log(classes)
 	const arbeidsgrupperStorgrupperSum = classes?.map((i) => i).reduce((prev, curr) => prev + curr, 0)
 	const arbeidsgrupper = userInputGroupe1?.Arbeidsgrupper || 0
 	const storgrupper = userInputGroupe1?.Storgrupper || 0
 
 	const userInputSum = arbeidsgrupper + storgrupper
-	const grupperProsent = (userInputSum * 100) / arbeidsgrupperStorgrupperSum
+	console.log(userInputGroupe1)
+	const grupperProsent = (userInputSum / arbeidsgrupperStorgrupperSum) * 100
+	console.log(grupperProsent)
 	return grupperProsent
 }
