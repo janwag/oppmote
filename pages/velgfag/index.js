dca5212de879b144eac5f0382bebf84a9c3dab19
sander <sandert.kodehode@gmail.com>
2023-02-01 22:23:17 +0100

    List sorted

--- pages/velgfag/index.js
+++ pages/velgfag/index.js
@@ -15,21 +15,25 @@ export async function getServerSideProps() {
 }
 
 const VelgFag = ({ data }) => {
-	const list = data.result
+	const list = data.result.map((item) => {
+		return item
+	})
+
+	list.sort((a, b) => (a.name > b.name ? 1 : -1))
+
+	const result = list.map((item) => {
+		return (
+			<li
+				className={s.link}
+				key={item._id}>
+				<Link href={'/velgfag/' + item.slug.current.toString()}>{item.name}</Link>
+			</li>
+		)
+	})
 	return (
 		<div className={s.container}>
 			<Header>Velg fag du vil regne ut</Header>
-			<ul className={s.list}>
-				{list.map((item) => {
-					return (
-						<li
-							className={s.link}
-							key={item._id}>
-							<Link href={'/velgfag/' + item.slug.current.toString()}>{item.name}</Link>
-						</li>
-					)
-				})}
-			</ul>
+			<ul className={s.list}>{result}</ul>
 		</div>
 	)
 }
