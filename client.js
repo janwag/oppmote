dcc70c45428aac0d2ad86c3d4b9741774aa4b5a7
Sander Tonheim <sandert.kodehode@gmail.com>
2023-02-01 20:06:23 +0100

    change to useCDN for Sanity

--- client.js
+++ client.js
@@ -3,5 +3,5 @@ import sanityClient from '@sanity/client'
 export default sanityClient({
 	projectId: 'qmgpu00g', // you can find this in sanity.json
 	dataset: 'production', // or the name you chose in step 1
-	useCdn: false, // `false` if you want to ensure fresh data
+	useCdn: true, // `false` if you want to ensure fresh data
 })
