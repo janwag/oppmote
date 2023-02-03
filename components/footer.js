92b9379c57471f90fe365b05fa7ae3c91381c989
sander <sandert.kodehode@gmail.com>
2023-02-03 16:22:10 +0100

    added credits

--- components/footer.js
+++ components/footer.js
@@ -1,12 +1,22 @@
 import Link from 'next/link'
-import styles from '../styles/Footer.module.css'
+import s from '../styles/Footer.module.css'
 export default function Footer() {
 	return (
-		<div className={styles.footer}>
-			<div className={styles.text}>
+		<div className={s.footer}>
+			<div className={s.text}>
 				<p>© Foks AS 2023</p>
+				<p>
+					Viktig info om tjenesten <Link href={'/info'}>her.</Link>
+				</p>
 			</div>
-			<Link href={'/info'}>Viktig info om tjenesten her.</Link>
+			<div className={s.credit}>
+				<p>
+					Utvikler: <br /> Sander Tonheim
+				</p>
+				<p>
+					Teknsik bistand: <br /> Ole Håvard Furuseth Bergan
+				</p>
+			</div>
 		</div>
 	)
 }
