a864d018c76bbd1630f4f625e5ba5ed94e5c81d2
sander <sandert.kodehode@gmail.com>
2023-02-04 17:46:56 +0100

    added headcomponent

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,32 @@
+import Head from 'next/head'
 import Link from 'next/link'
 import Header from '../components/Header'
 import s from '../styles/Home.module.css'
 function Home() {
 	return (
-		<div className={s.container}>
-			<div className={s.content}>
-				<Header>Regn ut din oppmøteprosent</Header>
-				<div className={s.link}>
-					<Link href='/velgfag'>Velg Fag</Link>
+		<>
+			<Head>
+				<meta
+					name='viewport'
+					content='width=device-width, initial-scale=1.0'
+				/>
+				<meta
+					property='og:title'
+					content='My page title'
+					key='title'
+				/>
+			</Head>
+
+			<div className={s.container}>
+				<div className={s.content}>
+					<Header>Regn ut din oppmøteprosent</Header>
+					<div className={s.link}>
+						<Link href='/velgfag'>Velg Fag</Link>
+					</div>
+					<div className={s.figure} />
 				</div>
-				<div className={s.figure} />
 			</div>
-		</div>
+		</>
 	)
 }
 export default Home
