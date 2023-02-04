af482774e5423d949694e5266e3d3aff3b9471b8
sander <sandert.kodehode@gmail.com>
2023-02-04 22:07:44 +0100

    iphone meta tags

--- pages/index.js
+++ pages/index.js
@@ -10,14 +10,22 @@ function Home() {
 					name='viewport'
 					content='width=device-width, initial-scale=1.0'
 				/>
-				<meta
-					property='og:title'
-					content='My page title'
-					key='title'
-				/>
+
 				<meta
 					name='apple-mobile-web-app-capable'
 					content='yes'
+				/>
+				<meta
+					name='apple-mobile-web-app-status-bar-style'
+					content='black-translucent'
+				/>
+				<meta
+					name='apple-mobile-web-app-title'
+					content='page title'
+				/>
+				<meta
+					name='apple-mobile-web-app-orientations'
+					content='portrait-any'
 				/>
 			</Head>
 
