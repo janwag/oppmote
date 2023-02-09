eddfc7556166693691b3a3d696695ee23c731208
Sander Tonheim <sandert.kodehode@gmail.com>
2023-02-09 15:22:45 +0100

    wrong way message

--- components/Layout.js
+++ components/Layout.js
@@ -5,20 +5,12 @@ import Head from 'next/head'
 export default function Layout({ children }) {
 	return (
 		<>
-			<Head>
-				<title>Nextjs</title>
-				<meta charset='UTF-8' />
-				<meta
-					name='viewport'
-					content='width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0'
-				/>
-				<meta
-					http-equiv='X-UA-Compatible'
-					content='ie=edge'
-				/>
-			</Head>
 			<Navbar />
 			<div className={s.content}>{children}</div>
+			<div className={s.wrongWay}>
+				<h1>Snu telefonen din tilbake for å fortsette</h1>
+			</div>
+
 			<Footer />
 		</>
 	)
