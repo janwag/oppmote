71c012c9087e234115440ea77459feb8a8a95a67
Sander Tonheim <sandert.kodehode@gmail.com>
2023-02-11 23:36:18 +0100

    new branche

--- pages/info.js
+++ pages/info.js
@@ -2,26 +2,28 @@ import groq from 'groq'
 import Link from 'next/link'
 import s from '../styles/info.module.css'
 import InfoHeader from '../components/infoHeader'
+import sanityClient from '../client'
 
 export default function Info({ data }) {
-	const about = data.result[0].about.map((item) => {
-		const aboutText = item.children.text
-		return aboutText
-	})
-	const whoMadeIt = data.result[0].whoMadeIt.map((item) => {
-		const whoMadeItText = item.children.text
-		return whoMadeItText
-	})
-	const userAgreement = data.result[0].userAgreement.map((item) => {
-		const userAgreementText = item.children.text
-		return userAgreementText
-	})
-	const lastUpdate = data.result[0].lastUpdate
-	console.log(lastUpdate)
+	console.log(data)
+	// const about = data.result.about.map((item) => {
+	// 	const aboutText = item.children.text
+	// 	return aboutText
+	// })
+	// const whoMadeIt = data.result[0].whoMadeIt.map((item) => {
+	// 	const whoMadeItText = item.children.text
+	// 	return whoMadeItText
+	// })
+	// const userAgreement = data.result[0].userAgreement.map((item) => {
+	// 	const userAgreementText = item.children.text
+	// 	return userAgreementText
+	// })
+	// const lastUpdate = data.result[0].lastUpdate
+	// console.log(lastUpdate)
 
 	return (
 		<div className={s.container}>
-			<h1>Viktig informasjon om applikasjonen</h1>
+			{/* <h1>Viktig informasjon om applikasjonen</h1>
 			<ul className={s.list}>
 				<li>
 					<Link href={'#section_1'}>Om tjenesten</Link>
@@ -54,22 +56,19 @@ export default function Info({ data }) {
 			{lastUpdate[0].children.text}
 			<Link href={'/pagelog'}>
 				<p>{lastUpdate[1].children.text}</p>
-			</Link>
+			</Link> */}
 		</div>
 	)
 }
 
 export async function getServerSideProps() {
-	const url = 'https://qmgpu00g.api.sanity.io/v1/data/query/production?query='
-	const data = await fetch(
-		url +
-			groq` *[_type == 'info']{
+	const data = await sanityClient.fetch(
+		groq` *[_type == 'info']{
   about[]{children[0]{text}},
   whoMadeIt[]{children[0]{text}},
 userAgreement[]{children[0]{text}},
-lastUpdate[]{children[0]{text}}
-}`
-	).then((data) => data.json())
+lastUpdate[]{children[0]{text}}}`
+	)
 
 	return {
 		props: {
